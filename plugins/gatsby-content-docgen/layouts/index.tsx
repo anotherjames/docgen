02c38054112cd6cb38827ecf7a6e69185f5f5e22
Paul Knopf <pauldotknopf@gmail.com>
2018-03-16 01:32:39 -0400

    Using tsx for layout.

--- plugins/gatsby-content-docgen/layouts/index.tsx
+++ plugins/gatsby-content-docgen/layouts/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from "react";
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
@@ -8,7 +8,7 @@ import './index.css'
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Docgen"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
@@ -27,9 +27,5 @@ const TemplateWrapper = ({ children }) => (
     </div>
   </div>
 )
-
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
 
 export default TemplateWrapper
