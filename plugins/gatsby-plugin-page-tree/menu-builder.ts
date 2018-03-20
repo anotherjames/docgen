e344bce3a0472eeab637e952bda36fc5490cc8f6
Paul Knopf <pauldotknopf@gmail.com>
2018-03-20 19:31:04 -0400

    Keep nodes without landing pages open.

--- plugins/gatsby-plugin-page-tree/menu-builder.ts
+++ plugins/gatsby-plugin-page-tree/menu-builder.ts
@@ -74,8 +74,13 @@ export function buildMenuFromNodes(nodes: Array<GatsbyNode>, selectedPath: strin
 
     // Now that we built the entire tree, let's remove the nodes that don't matter.
     const collapseChildren = (item: MenuItem) => {
-        if(!item.active && !item.selected) {
-            item.children = [];
+        // We only want to consider collapsing children if this current node can be selected.
+        // If it isn't a valid page, then the user can't navigate to it to see it's children.
+        if(!item.isEmptyParent) {
+            // Collapse children if this node isn't in the active selected path.
+            if(!item.active && !item.selected) {
+                item.children = [];
+            }
         }
         for(let child of item.children) {
             collapseChildren(child);
