478ad954942c5dc1b8378a32386e4b4c0c83d5de
Paul Knopf <pauldotknopf@gmail.com>
2018-03-23 02:29:15 -0400

    Prevent sidebar from collapsing in desktop mode.

--- plugins/gatsby-content-docgen/layouts/index.tsx
+++ plugins/gatsby-content-docgen/layouts/index.tsx
@@ -1,27 +1,66 @@
 import * as React from "react";
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
+import Link from 'gatsby-link'
 import Header from '../components/Header'
 import './assets/styles.less'
 
-export default (props) => {
-  return (
-    <div>
-      <Helmet title={props.data.site.siteMetadata.title} />
-      <header className="main-header">
-        <nav className="navbar navbar-static-top">
-        <a href="#" className="sidebar-toggle" data-toggle="push-menu" role="button">
-          <span className="sr-only">Toggle navigation</span>
-        </a>
-        </nav>
-      </header>
-      {props.children()}
+interface LayoutProps {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string
+      }
+    }
+  };
+  children: any;
+}
+
+interface LayoutState {
+  sidebarOpen: boolean
+}
+
+export default class DefaultLayout extends React.Component<LayoutProps, LayoutState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarOpen: false
+    }
+  }
+  onBarsClick = (e) => {
+    this.setState({
+      sidebarOpen: !this.state.sidebarOpen
+    })
+  }
+  render() {
+    let bodyClass = 'skin-blue'
+    if(!this.state.sidebarOpen) {
+      bodyClass += ' sidebar-collapse';
+    } else {
+      bodyClass += ' sidebar-open';
+    }
+    return (
       <div>
-        Site props: {JSON.stringify(props)}
+        <Helmet title={this.props.data.site.siteMetadata.title}>
+          <body className={bodyClass} />
+        </Helmet>
+        <header className="main-header">
+          <Link to="/" className="logo">
+            <span className="logo-lg"><b>Doc</b>Gen</span>
+          </Link>
+          <nav className="navbar navbar-static-top">
+            <a href="javasript:void(0);" onClick={this.onBarsClick} className="sidebar-toggle"></a>
+          </nav>
+        </header>
+        <aside className="main-sidebar">
+        sidebar
+        </aside>
+        <div className="content-wrapper">
+          {this.props.children()}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export const query = graphql`
@@ -33,55 +72,3 @@ export const query = graphql`
     }
   }
 `
-{/* <header class="main-header">
-    <nav class="navbar navbar-static-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <a href="../../index2.html" class="navbar-brand"><b>Admin</b>LTE</a>
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
-            <i class="fa fa-bars"></i>
-          </button>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Link</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li><a href="#">Separated link</a></li>
-                <li class="divider"></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-          <form class="navbar-form navbar-left" role="search">
-            <div class="form-group">
-              <input type="text" class="form-control" id="navbar-search-input" placeholder="Search">
-            </div>
-          </form>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Link</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li><a href="#">Separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-      </div>
-      <!-- /.container-fluid -->
-    </nav>
-  </header> */}
