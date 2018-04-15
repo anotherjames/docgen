9ca734ebe4b4965e5836a60515e6ff03dfe1f6b4
Paul Knopf <pauldotknopf@gmail.com>
2018-04-15 17:40:38 -0400

    Not embedding gatsby anymore.

--- packages/docgen-gatsby/src/index.ts
+++ packages/docgen-gatsby/src/index.ts
@@ -1,71 +1,51 @@
-import * as build from 'gatsby/dist/commands/build'
-import * as bootstrap from 'gatsby/dist/bootstrap'
-import * as serve from 'gatsby/dist/commands/serve'
-import * as develop from 'gatsby/dist/commands/develop'
 import * as fs from 'fs'
 import * as fsExtra from 'fs-extra'
 import * as path from 'path';
 import * as util from 'util'
+import { spawn } from 'child_process';
 
 const exists = util.promisify(fs.exists);
 const unlink = util.promisify(fs.unlink);
 const copy = util.promisify(fsExtra.copy);
-const remove = util.promisify(fsExtra.remove);
 
-export async function buildDirectory(): Promise<void> {
-    let sourceConfigFileLocation = path.join(__dirname, 'default-gatsby-config.js');
-    let destConfigLocation = path.join(process.cwd(), 'gatsby-config.js');
-
-    if (await exists(destConfigLocation)) {
-        await unlink(destConfigLocation);
-    }
-
-    await copy(sourceConfigFileLocation, destConfigLocation);
-
-    await build({
-        directory: process.cwd(),
-        sitePackageJson: {
-            name: "docgen"
-        },
-        useYarn: true
-    });
-
-    return Promise.resolve();
-}
-
-export function serveDirectory() {
-    serve({
-        directory: process.cwd(),
-        port: 8000
-    });
-}
-
-export async function developDirectory() {
-    let sourceConfigFileLocation = path.join(__dirname, 'default-gatsby-config.js');
-    let destConfigLocation = path.join(process.cwd(), 'gatsby-config.js');
-
-    if (await exists(destConfigLocation)) {
-        await unlink(destConfigLocation);
-    }
-
-    await copy(sourceConfigFileLocation, destConfigLocation);
-
-    develop({
-        directory: process.cwd(),
-        sitePackageJson: {
-            name: "docgen"
-        },
-        port: 8000
-    });
+async function copyFile(sourceFile: string, destinationFile: string): Promise<boolean> {
+    return true;
 }
 
 export async function cleanDirectory() {
     let cacheDir = path.join(process.cwd(), '.cache');
     let buildDir = path.join(process.cwd(), 'public');
     if(await exists(cacheDir)) {
-        remove(cacheDir);
+        await fsExtra.remove(cacheDir);
     }
     if(await exists(buildDir)) {
-        remove(buildDir);
+        await fsExtra.remove(buildDir);
     }
+}
+
+async function prepareDirectory() {
+    let currentDirectory = process.cwd();
+    let wasChanged = await copyFile(path.join(__dirname, 'default-gatsby-config.js'), path.join(currentDirectory, 'gatsby-config.js'));
+    wasChanged = wasChanged || await copyFile(path.join(__dirname, 'default-gatsby-config.js'), path.join(currentDirectory, 'package.json'));
+    if (wasChanged) {
+        // Re-install the node_modules
+        fsExtra.rmdir(path.join(currentDirectory, 'node_modules'));
+        await spawn("npm install");
+    }
+    await cleanDirectory();
+}
+
+export async function buildDirectory() {
+    await prepareDirectory();
+    await spawn('./node_modules/.bin/gatsby build');
+}
+
+export async function serveDirectory() {
+    await prepareDirectory();
+    await spawn('./node_modules/.bin/gatsby serve');
+}
+
+export async function developDirectory() {
+    await prepareDirectory();
+    await spawn('./node_modules/.bin/gatsby develop');
 }
